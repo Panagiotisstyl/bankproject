9ff3b1c6d43c884924f377150e2cc8c5b3a99080
Panagiotisstyl <pstyli05@ucy.ac.cy>
2025-07-11 10:05:33 +0300

    No logic in controllers

--- src/main/java/com/root/bankproject/controllers/AccountsRestController.java
+++ src/main/java/com/root/bankproject/controllers/AccountsRestController.java
@@ -3,9 +3,6 @@ package com.root.bankproject.controllers;
 import com.root.bankproject.converters.AccountsConverter;
 import com.root.bankproject.dtos.AccountResponseDto;
 import com.root.bankproject.dtos.AccountsDto;
-import com.root.bankproject.entities.Accounts;
-import com.root.bankproject.services.AccountsService;
-import com.root.bankproject.validations.AccountValidation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,33 +13,26 @@ import java.util.List;
 @RequestMapping("api/v1")
 public class AccountsRestController {
 
-    private final AccountsService accountsService;
     private final AccountsConverter accountsConverter;
-    private final AccountValidation accountValidation;
 
     @GetMapping("/accounts")
     public List<AccountResponseDto> findALlAccounts() {
-        return accountsConverter.toDtoList(accountsService.findAll());
+        return accountsConverter.findALlConvert();
     }
 
     @GetMapping("/accounts/{accountId}")
     public AccountResponseDto findAccountById(@PathVariable int accountId) {
-        return accountsConverter.toResponseDto(accountsService.findById(accountId));
+        return accountsConverter.findByIdConvert(accountId);
     }
 
     @PostMapping("/accounts")
     public AccountResponseDto registerAccount(@RequestBody AccountsDto accountsDto) {
-        return accountsConverter.toResponseDto(accountsService.save(accountsConverter.toEntity(accountsDto)));
+        return accountsConverter.registerAccountConvert(accountsDto);
     }
 
     @PostMapping("/accounts/addUser/{userId}")
     public void addUser(@RequestBody AccountResponseDto accountResponseDto, @PathVariable int userId) {
-        
-        AccountsDto dto=accountValidation.validateUsersAccount(accountResponseDto,userId);
-        Accounts accToUpdate=accountsConverter.toEntity(dto, accountsService.findById(accountResponseDto.getId()));
-        accountsService.save(accToUpdate);
-
-
+        accountsConverter.addUserConvert(accountResponseDto,userId);
 
     }
 }
