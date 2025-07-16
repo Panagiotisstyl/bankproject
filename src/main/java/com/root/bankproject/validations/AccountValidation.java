c7e2a9fbdd91813951155d29b3fca8dbb2d1cd17
Panagiotisstyl <pstyli05@ucy.ac.cy>
2025-07-16 08:43:20 +0300

    Fixed things mentioned yesterday -Made methods to add or take from balance inside Account -Changed the way I added a user to an account's list -In validateUserAccount now I just send an account object

--- src/main/java/com/root/bankproject/validations/AccountValidation.java
+++ src/main/java/com/root/bankproject/validations/AccountValidation.java
@@ -3,8 +3,6 @@ package com.root.bankproject.validations;
 
 import com.root.bankproject.entities.Account;
 import com.root.bankproject.enums.TypeAccount;
-import com.root.bankproject.repositories.AccountsRepository;
-import com.root.bankproject.services.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +11,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class AccountValidation {
 
-    private final AccountsRepository accountsRepository;
-    private final UsersService usersService;
 
-    public void validateUsersAccount(int accId, int userId){
-
-            Account account= accountsRepository.findById(accId).orElseThrow(()->new RuntimeException("User not found"));
-            usersService.findById(userId);
+    public void validateUsersAccount(Account account){
 
             if(account.getTypeAccount()== TypeAccount.SINGLE && !account.getUsers().isEmpty()){
                 throw new RuntimeException("Cannot add user, account type is single");
