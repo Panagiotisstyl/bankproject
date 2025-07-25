49288e1722b3d9b6850d77b96cc2d9448ed69488
Panagiotisstyl <pstyli05@ucy.ac.cy>
2025-07-25 15:08:23 +0300

    Added topics

--- src/main/java/com/root/bankproject/kafka/KafkaProducerConfig.java
+++ src/main/java/com/root/bankproject/kafka/KafkaProducerConfig.java
@@ -1,6 +1,8 @@
 package com.root.bankproject.kafka;
 
+import com.root.bankproject.dtos.AccountsDto;
 import com.root.bankproject.dtos.UserResponseDto;
+import com.root.bankproject.dtos.UsersDto;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import com.root.bankproject.dtos.AccountResponseDto;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -18,7 +20,7 @@ import java.util.Map;
 public class KafkaProducerConfig{
 
     @Bean
-    public ProducerFactory<String, AccountResponseDto> producerFactoryAccount(){
+    public ProducerFactory<String, AccountsDto> producerFactoryAccount(){
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -27,7 +29,7 @@ public class KafkaProducerConfig{
     }
 
     @Bean
-    public ProducerFactory<String, UserResponseDto> producerFactoryUser(){
+    public ProducerFactory<String, UsersDto> producerFactoryUser(){
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -36,12 +38,41 @@ public class KafkaProducerConfig{
     }
 
     @Bean
-    public KafkaTemplate<String, AccountResponseDto> kafkaTemplateAccount(){
+    public ProducerFactory<String, AccountResponseDto> producerFactoryAccountResponse(){
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public ProducerFactory<String, UserResponseDto> producerFactoryUserResponse(){
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, AccountsDto> kafkaTemplateAccount(){
         return new KafkaTemplate<>(producerFactoryAccount());
     }
 
     @Bean
-    public KafkaTemplate<String, UserResponseDto> kafkaTemplateUser(){
+    public KafkaTemplate<String, UsersDto> kafkaTemplateUser(){
         return new KafkaTemplate<>(producerFactoryUser());
     }
+
+    @Bean
+    public KafkaTemplate<String, AccountResponseDto> kafkaTemplateAccountResponse(){
+        return new KafkaTemplate<>(producerFactoryAccountResponse());
+    }
+
+    @Bean
+    public KafkaTemplate<String, UserResponseDto> kafkaTemplateUserResponse(){
+        return new KafkaTemplate<>(producerFactoryUserResponse());
+    }
+
 }
