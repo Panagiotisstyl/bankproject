49dfb3850f769a61d61d020bc9eca3a66d7a384b
Panagiotisstyl <pstyli05@ucy.ac.cy>
2025-07-29 10:55:36 +0300

    Updated Used logger and made it generic for the whole project

--- src/main/java/com/root/bankproject/kafka/KafkaConsumer.java
+++ src/main/java/com/root/bankproject/kafka/KafkaConsumer.java
@@ -1,36 +1,25 @@
 package com.root.bankproject.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics="account.created",groupId="groupA")
-    private void listenAccount(String jsonMessage){
-        System.out.println("Received message:"+jsonMessage);
-    }
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    @KafkaListener(topics="user.created", groupId = "groupB")
-    private void listenUser(String jsonMessage){
-        System.out.println("Received message:"+jsonMessage);
+    @KafkaListener(topics= {
+            "account.created",
+            "user.created",
+            "deposit.money",
+            "withdraw.money",
+            "user.addedToAccount"
+    }, groupId = "allTopics")
+    private void listenToALlTopics(String jsonMessage){
+        log.info("Received message"+jsonMessage);
     }
-
-    @KafkaListener(topics="deposit.money", groupId = "groupC")
-    private void listenAccounDeposit(String jsonMessage){
-        System.out.println("Deposit money:"+jsonMessage);
-    }
-
-    @KafkaListener(topics="withdraw.money", groupId = "groupD")
-    private void listenAccountWithdraw(String jsonMessage){
-        System.out.println("Withdraw money:"+jsonMessage);
-    }
-
-    @KafkaListener(topics="user.addedToAccount", groupId = "groupE")
-    private void listenAccountAddUser(String jsonMessage){
-        System.out.println("Added user:"+jsonMessage);
-    }
-
 
 
 }
