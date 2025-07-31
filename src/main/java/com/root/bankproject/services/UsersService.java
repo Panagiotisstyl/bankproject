9790739c0d50bbd2f476ae51ddfb47c42443bc43
Panagiotisstyl <pstyli05@ucy.ac.cy>
2025-07-31 11:31:11 +0300

    Redis cache

--- src/main/java/com/root/bankproject/services/UsersService.java
+++ src/main/java/com/root/bankproject/services/UsersService.java
@@ -3,6 +3,9 @@ package com.root.bankproject.services;
 import com.root.bankproject.entities.User;
 import com.root.bankproject.repositories.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,23 +16,29 @@ public class UsersService {
 
     private final UsersRepository usersRepository;
 
+    @Cacheable(value="userList")
     public List<User> findALl(){
         return usersRepository.findAll();
     }
 
+    @Cacheable(value="user",key="#id")
     public User findById(Integer id){
         return usersRepository.findById(id).orElseThrow(()->new RuntimeException("User not found"));
     }
 
+    @CachePut(value="user",key="#user.id")
+    @CacheEvict(value = "userList", allEntries = true)
     public User save(User user){
         return usersRepository.save(user);
     }
 
 
+    @Cacheable(value="user",key="#email")
     public User findByEmail(String email) {
         return usersRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
     }
 
+    @Cacheable(value="accountUserList",key="#accountId")
     public List<User> findByAccountId(int accountId) {
         return usersRepository.findAllByAccounts_Id(accountId);
     }
